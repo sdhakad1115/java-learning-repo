1851e3f34517ea38061dfae020c7ff9546c8fc90
Shriram Dhakad <sdhakad2225@gmail.com>
2021-04-21 23:56:15 +0530

    jwt initial project

---
+++ sprint-boot-jwt-demo/src/main/java/com/sprintboot/jwt/token/SprintBootJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sprintboot.jwt.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootJwtDemoApplication.class, args);
+	}
+
+}
